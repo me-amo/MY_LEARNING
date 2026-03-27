46e1acb0335f55652acd92b3dbe7547fcdfe9ae3
Emmanuel Amodoi <amodoie@gmail.com>
2026-03-27 15:24:13 +0300

    Included the switch statement in here

--- META_FSD/js_learning/for.js
+++ META_FSD/js_learning/for.js
@@ -13,3 +13,19 @@ for(i = 1; i <= 10; i++){
         console.log(i)
     }
 }
+//Testing the switch statement
+for(i = 1; i <= 10; i++){
+    switch(i){
+case 1:
+        console.log("Gold medal")
+        break;
+    case 2:
+        console.log("Silver medal")
+        break;
+    case 3:
+        console.log("Bronze medal")
+        break;
+    default:
+        console.log(i)
+}
+}
